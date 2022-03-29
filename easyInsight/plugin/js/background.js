da6bb9d36b54d0b641043032e678c67bc215c4b2
sujithasuresh13 <31007519+sujithasuresh13@users.noreply.github.com>
2022-03-29 18:52:56 +0530

    Update background.js for twitter support

--- easyInsight/plugin/js/background.js
+++ easyInsight/plugin/js/background.js
@@ -4,6 +4,8 @@ chrome.runtime.onMessage.addListener(function(message, sender, callback) {
 		chrome.storage.local.set({'easyInsightTargetSelector': "[aria-label='Message body']"})
 	} else if (message.target == "DEMO-TEXT-BOX") {
 		chrome.storage.local.set({'easyInsightTargetSelector': "#autocomplete-textarea"});
+	} else if (message.target == "twitter") {
+		chrome.storage.local.set({'easyInsightTargetSelector': "[aria-label='Tweet text']"})
 	}
 	chrome.scripting.executeScript({
 		files: ['js/jquery.min.js', 'js/ajax.js', 'js/jquery-textcomplete.min.js', 'js/main.js'], 
