508994403724189399a8bea843400b8f4a220852
vakodali <varsha.kodali@oracle.com>
2022-03-30 21:16:04 -0700

    fix: comment out rendering elements!

--- easyInsight/plugin/js/popup.js
+++ easyInsight/plugin/js/popup.js
@@ -11,25 +11,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function dumpMetrics() {
   if (ul) {
-    for (var i = 0; metrics.length; i++)
-    {
-        var li = document.createElement("li");  
-        li.classList.add("w3-panel");
-        li.classList.add("w3-card");
-        var a = document.createElement("a");
-        var btn = document.createElement("button");
-        btn.innerHTML = "UnPin";
-        btn.onclick = function(metric) {
-          unPin(metric);
-        }(metrics[i]);
-        a.href = 'https://www.w3schools.com';
-        var img = document.createElement("img");
-        img.src="images/img6.png";
-        a.innerHTML = metrics[0];
-        li.appendChild(a);
-        li.appendChild(img);
-        ul.appendChild(li);
-    }
+    // for (var i = 0; metrics.length; i++)
+    // {
+    //     var li = document.createElement("li");  
+    //     li.classList.add("w3-panel");
+    //     li.classList.add("w3-card");
+    //     var a = document.createElement("a");
+    //     var btn = document.createElement("button");
+    //     btn.innerHTML = "UnPin";
+    //     btn.onclick = function(metric) {
+    //       unPin(metric);
+    //     }(metrics[i]);
+    //     a.href = 'https://www.w3schools.com';
+    //     var img = document.createElement("img");
+    //     img.src="images/img6.png";
+    //     a.innerHTML = metrics[0];
+    //     li.appendChild(a);
+    //     li.appendChild(img);
+    //     ul.appendChild(li);
+    // }
   }
 }
 
