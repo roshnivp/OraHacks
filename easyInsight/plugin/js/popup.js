d729632a7a080cd1c6f6f211dde07714c226c621
Roshni Velluva Puthanidam <roshniknambiar@gmail.com>
2022-03-31 12:01:20 -0700

    fix searc, icons

--- easyInsight/plugin/js/popup.js
+++ easyInsight/plugin/js/popup.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
   console.log("popup.js - $(document).ready");
 
   // On Value Change in the Search Bar
-  document.getElementById("easyInsight-search-pinboard").addEventListener('change', (event) => {
+  document.getElementById("easyInsight-search-pinboard").addEventListener('input', (event) => {
     let searchTest = document.getElementById("easyInsight-search-pinboard").value;
     console.log("Typing Something", event, searchTest);
     let filtered_metrics = metrics.filter(m => m.toUpperCase().indexOf(searchTest.toUpperCase()) !== -1);
@@ -65,7 +65,7 @@ function dumpMetrics(inputmerics) {
       btn_elem1.style="margin-left: 10px;border-radius: 15px;";
       let btn_span_elem = document.createElement("span");
       btn_span_elem.classList.add("glyphicon");
-      btn_span_elem.classList.add("glyphicon-trash");
+      btn_span_elem.classList.add("glyphicon-minus-sign");
       // btn_span_elem.innerHTML = "Remove";
       btn_elem1.appendChild(btn_span_elem);
       let id = inputmerics[i];
@@ -82,7 +82,7 @@ function dumpMetrics(inputmerics) {
       btn_elem2.style="margin-left: 10px;border-radius: 15px;";
       btn_span_elem = document.createElement("span");
       btn_span_elem.classList.add("glyphicon");
-      btn_span_elem.classList.add("glyphicon-link");
+      btn_span_elem.classList.add("glyphicon-pushpin");
       // btn_span_elem.innerHTML = "Dashboard";
       btn_elem2.appendChild(btn_span_elem);
       let url = rawValue.dashboardurl;
@@ -109,6 +109,24 @@ function dumpMetrics(inputmerics) {
         window.open(url, '_blank');
       };
 
+      let btn_elem4 = document.createElement("button");
+      btn_elem4.classList.add("easyinsight-delete-btn");
+      btn_elem4.classList.add("btn");
+      btn_elem4.classList.add("btn-default");
+      btn_elem4.classList.add("btn-sm");
+      btn_elem4.style="margin-left: 10px;border-radius: 15px;";
+      btn_span_elem = document.createElement("span");
+      btn_span_elem.classList.add("glyphicon");
+      btn_span_elem.classList.add("glyphicon-thumbs-up");
+        // btn_span_elem.innerHTML = "Dashboard";
+      btn_elem4.appendChild(btn_span_elem);
+      var count = 1;
+      btn_elem4.onclick = function() {
+          console.log(count + " likes");
+          count = count + 1 ;
+          btn_elem4.style="margin-left: 10px;border-radius: 15px; font-weight: bold; color: blue;";
+      };
+
       // <button type="button" class="btn btn-default btn-sm">
       // <span class="glyphicon glyphicon-trash"></span> Remove Pin
       // </button>
@@ -128,6 +146,7 @@ function dumpMetrics(inputmerics) {
       li_elm.appendChild(btn_elem2);
       li_elm.appendChild(btn_elem1);
       li_elm.appendChild(btn_elem3);
+      li_elm.appendChild(btn_elem4);
       ul.appendChild(li_elm);
     }
     console.log(ul);
