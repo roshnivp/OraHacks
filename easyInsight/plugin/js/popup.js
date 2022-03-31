5af211e5e2e96d24d3db953ac864ba6600d4b59b
Roshni Velluva Puthanidam <roshniknambiar@gmail.com>
2022-03-30 17:30:44 -0700

    pop up changes added

--- easyInsight/plugin/js/popup.js
+++ easyInsight/plugin/js/popup.js
@@ -4,11 +4,11 @@
 // license that can be found in the LICENSE file or at
 // https://developers.google.com/open-source/licenses/bsd
 
-// Search the bookmarks when entering the search keyword.
-$('#search').change(function () {
-  $('#bookmarks').empty();
-  dumpBookmarks($('#search').val());
-});
+//// Search the bookmarks when entering the search keyword.
+//$('#search').change(function () {
+//  $('#bookmarks').empty();
+//  dumpBookmarks($('#search').val());
+//});
 
 // Traverse the bookmark tree, and print the folder and nodes.
 function dumpBookmarks(query) {
@@ -47,111 +47,18 @@ function dumpNode(bookmarkNode, query) {
     });
 
     var span = $('<span>');
-    var options = bookmarkNode.children ?
-      $('<span>[<a href="#" id="addlink">Add</a>]</span>') :
-      $('<span>[<a id="editlink" href="#">Edit</a> <a id="deletelink" ' +
-        'href="#">Delete</a>]</span>');
-    var edit = bookmarkNode.children ? $('<table><tr><td>Name</td><td>' +
-      '<input id="title"></td></tr><tr><td>URL</td><td><input id="url">' +
-      '</td></tr></table>') : $('<input>');
+    var options = $('<span>[<a href="#" id="addlink">Add</a>]</span>') ;
 
-      // Show add and edit links when hover over.
+    // Show add and edit links when hover over.
     span.hover(function () {
       span.append(options);
-      $('#deletelink').click(function (event) {
-        console.log(event)
-        $('#deletedialog').empty().dialog({
-          autoOpen: false,
-          closeOnEscape: true,
-          title: 'Confirm Deletion',
-          modal: true,
-          show: 'slide',
-          position: {
-            my: "left",
-            at: "center",
-            of: event.target.parentElement.parentElement
-          },
-          buttons: {
-            'Yes, Delete It!': function () {
-              chrome.bookmarks.remove(String(bookmarkNode.id));
-              span.parent().remove();
-              $(this).dialog('destroy');
-            },
-            Cancel: function () {
-              $(this).dialog('destroy');
-            }
-          }
-        }).dialog('open');
-      });
-      $('#addlink').click(function (event) {
-        edit.show();
-        $('#adddialog').empty().append(edit).dialog({
-          autoOpen: false,
-          closeOnEscape: true,
-          title: 'Add New Bookmark',
-          modal: true,
-          show: 'slide',
-          position: {
-            my: "left",
-            at: "center",
-            of: event.target.parentElement.parentElement
-          },
-          buttons: {
-            'Add': function () {
-              edit.hide();
-              chrome.bookmarks.create({
-                parentId: bookmarkNode.id,
-                title: $('#title').val(), url: $('#url').val()
-              });
-              $('#bookmarks').empty();
-              $(this).dialog('destroy');
-              window.dumpBookmarks();
-            },
-            'Cancel': function () {
-              edit.hide();
-              $(this).dialog('destroy');
-            }
-          }
-        }).dialog('open');
-      });
-      $('#editlink').click(function (event) {
-        edit.show();
-        edit.val(anchor.text());
-        $('#editdialog').empty().append(edit).dialog({
-          autoOpen: false,
-          closeOnEscape: true,
-          title: 'Edit Title',
-          modal: true,
-          show: 'fade',
-          position: {
-            my: "left",
-            at: "center",
-            of: event.target.parentElement.parentElement
-          },
-          buttons: {
-            'Save': function () {
-              edit.hide();
-              chrome.bookmarks.update(String(bookmarkNode.id), {
-                title: edit.val()
-              });
-              anchor.text(edit.val());
-              options.show();
-              $(this).dialog('destroy');
-            },
-            'Cancel': function () {
-              edit.hide();
-              $(this).dialog('destroy');
-            }
-          }
-        }).dialog('open');
-      });
       options.fadeIn();
     },
-
       // unhover
-      function () {
-        options.remove();
-      }).append(anchor);
+   function () {
+      options.remove();
+   }).append(anchor);
+
   }
 
   var li = $(bookmarkNode.title ? '<li>' : '<div>').append(span);
