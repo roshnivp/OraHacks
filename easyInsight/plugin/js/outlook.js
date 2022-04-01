94201ff584cd1fcc170b925ce1e2354fa5539390
vakodali <varsha.kodali@oracle.com>
2022-04-01 10:43:40 -0700

    fix: dropdown should show!

--- easyInsight/plugin/js/outlook.js
+++ easyInsight/plugin/js/outlook.js
@@ -1,10 +1,10 @@
-document.addEventListener("click", () => {
-	if (!selectorElement) {
+document.addEventListener("click", (ev) => {
+	// if (!selectorElement) {
 		selectorElement = $("[aria-label='Message body']");
 		selectorElement.textcomplete([mentionStrategy], {
 			debounce: SEARCH_MS_DEBOUNCE,
 			maxCount: AUTOCOMPLETE_ITEMS_DISPLAYED,
 			adapter: $.fn.textcomplete.HTMLContentEditable
 		});
-	}
+	// }
 });
