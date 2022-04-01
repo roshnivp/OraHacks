503b9c4fb95045d88681151764b2ac0fd0911447
vakodali <varsha.kodali@oracle.com>
2022-03-31 22:41:41 -0700

    fix: remove console log in twitter!

--- easyInsight/plugin/js/twitter.js
+++ easyInsight/plugin/js/twitter.js
@@ -1,9 +1,7 @@
 window.addEventListener("click", () => {
 	if (!selectorElement) {
 		selectorElement = $("[aria-label='Tweet text']");
-		chrome.storage.sync.set({'isTwitter': true}, function() {
-			console.log('isTwitter Value is set to ' + value);
-		  });
+		chrome.storage.sync.set({'isTwitter': true});
 		selectorElement.textcomplete([mentionStrategy], {
 			debounce: SEARCH_MS_DEBOUNCE,
 			maxCount: AUTOCOMPLETE_ITEMS_DISPLAYED,
